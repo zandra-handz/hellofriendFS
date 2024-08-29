baf777ba80456fca342f1c46c1b5ce39cc936e40
zandra-handz <taylorcolegrove@gmail.com>
2024-08-28 22:19:13 -0400

    oops pushed a buggy view. fixed!

--- hellofriend/hfroot/hfroot/settings.py
+++ hellofriend/hfroot/hfroot/settings.py
@@ -224,9 +224,6 @@ CORS_ALLOW_CREDENTIALS = True
 
 # actually not sure if this is the right access key but it's the right bucket
 
- 
-  
-
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
--- hellofriend/hfroot/users/views.py
+++ hellofriend/hfroot/users/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, response, status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from rest_framework.views import APIView 
