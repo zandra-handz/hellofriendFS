92091005e09d87184208c5e3840cc08af34b58b9
zandra-handz <taylorcolegrove@gmail.com>
2024-08-31 11:08:11 -0400

    changed access key lifetime to help determine what is causing my 401 errors

--- hellofriend/hfroot/hfroot/settings.py
+++ hellofriend/hfroot/hfroot/settings.py
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(hours=2),
     "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=7),
 }
 
