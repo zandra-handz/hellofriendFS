57a22acde80639297391baae9f733e9a28e1663b
zandra_handz <taylorcolegrove@gmail.com>
2026-05-05 00:35:39 -0400

    changed base url again

--- gecko-socket-rust/src/main.rs
+++ gecko-socket-rust/src/main.rs
@@ -1457,7 +1457,7 @@ type ClientId = String;
 type RoomName = String;
 type Tx = mpsc::UnboundedSender<Message>;
 
-const DJANGO_BASE_URL: &str = "http://127.0.0.1";
+const DJANGO_BASE_URL: &str = "https://badrainbowz.com";
 
 const BINARY_OUTBOUND_ACTIONS: &[&str] = &[
     "gecko_coords",
